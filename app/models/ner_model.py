98d2ac722722c277d56d6a90dccec52c09f020a9
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2024-06-16 16:54:41 -0400

    Using transformers cache

--- app/models/ner_model.py
+++ app/models/ner_model.py
@@ -7,9 +7,13 @@ import os
 
 load_dotenv()
 
+os.environ['TRANSFORMERS_CACHE'] = '/tmp/huggingface_cache'
+
+os.makedirs(os.environ['TRANSFORMERS_CACHE'], exist_ok=True)
+
 api_key = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
-login(token=api_key)
+login(token=api_key, add_to_git_credential=False)
 
 class NerModel():
     def __init__(self):
@@ -18,8 +22,8 @@ class NerModel():
 
     def load_model(self, category, model_name):
         model = self.__assign_model(category, model_name)
-        self.__tokenizer = AutoTokenizer.from_pretrained(model)
-        self.__model = AutoModelForTokenClassification.from_pretrained(model)
+        self.__tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=api_key)
+        self.__model = AutoModelForTokenClassification.from_pretrained(model, use_auth_token=api_key)
             
     def get_inference(self, text):
         nlp = pipeline("ner", model=self.__model, tokenizer=self.__tokenizer, aggregation_strategy="first")
