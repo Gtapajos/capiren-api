2d9b1659202a1f7d63b5b63e72434651f5864c30
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2024-06-16 17:20:52 -0400

    temporary update

--- app/models/ner_model.py
+++ app/models/ner_model.py
@@ -1,6 +1,12 @@
 from transformers import AutoTokenizer
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
+from huggingface_hub import login
+import os
+
+api_key = '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'
+
+login(token='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', add_to_git_credential=False)
 
 class NerModel():
     def __init__(self):
@@ -9,8 +15,8 @@ class NerModel():
 
     def load_model(self, category, model_name):
         model = self.__assign_model(category, model_name)
-        self.__tokenizer = AutoTokenizer.from_pretrained(model)
-        self.__model = AutoModelForTokenClassification.from_pretrained(model)
+        self.__tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=api_key)
+        self.__model = AutoModelForTokenClassification.from_pretrained(model, use_auth_token=api_key)
             
     def get_inference(self, text):
         nlp = pipeline("ner", model=self.__model, tokenizer=self.__tokenizer, aggregation_strategy="first")
